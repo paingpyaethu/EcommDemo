0f8283df73456757861d20d709fc002a221670df
paingpyaethu <86873013+paingpyaethu@users.noreply.github.com>
2024-12-31 21:08:04 +0630

    remove light and dark icon

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -3,14 +3,11 @@ import {Pressable, View} from 'react-native';
 import {ThemedText} from '@/components/atoms';
 import {SafeScreen} from '@/components/template';
 import ProductList from '@/components/organisms/ProductList/ProductList';
-import {ContrastIcon} from '@/utils/svg/icon.common';
-import {useColorScheme} from 'nativewind';
 import { useDispatch } from 'react-redux';
 import { clearData } from '@/store/features/user/userSlice';
 import { resetAndNavigate } from '@/utils/navigationUtil';
 
 const HomeScreen = () => {
-  const {colorScheme, setColorScheme} = useColorScheme();
   const dispatch = useDispatch();
   return (
     <SafeScreen>
@@ -19,12 +16,6 @@ const HomeScreen = () => {
           <ThemedText variant={'primary'} size={'xl_24'} weight={'bold'}>
             Discover
           </ThemedText>
-          <Pressable
-            onPress={() =>
-              setColorScheme(colorScheme === 'light' ? 'dark' : 'light')
-            }>
-            <ContrastIcon colorScheme={colorScheme} stroke={colorScheme === 'light' ? "#000" : "#FFF"}/>
-          </Pressable>
           {/* Temporary */}
           <Pressable onPress={() => {
             dispatch(clearData());
